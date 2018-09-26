a56d0476e39bfc9d2f6a8eaff1fef46fc6c01931
Lucas Lombardo <Lucasl@bu.edu>
2018-09-26 17:17:33 -0400

    adds context api

--- src/App.js
+++ src/App.js
@@ -1,32 +1,54 @@
 import React, { Component, Fragment } from 'react';
-// eslint-disable-next-line
-import { Toggle } from "Utilities";
-// eslint-disable-next-line
-import { Modal } from "Elements";
+/* eslint react/no-unused-state: 0 react/no-multi-comp:0 */
+/* eslint import/no-unresolved: 0 react/prop-types: 0 */
+// TODO: fix proptypes, fix unresolved path linting
+import { Toggle } from 'Utilities';
+import { Modal } from 'Elements';
+import User from './User';
 import logo from './logo.svg';
 import './App.css';
+import { UserContext } from './UserContext';
+
+class UserProvider extends Component {
+  state = {
+    id: 123,
+    name: 'Scott',
+    email: 'Scott@LUT.com',
+  };
+
+  render() {
+    return (
+      <UserContext.Provider value={{ user: this.state }}>
+        {this.props.children}
+      </UserContext.Provider>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Toggle>
-          {({ on, toggle }) => (
-            <Fragment>
-              <button type="button" onClick={toggle}>
-                Show/Hide
-              </button>
-              <Modal on={on} toggle={toggle}>
-                <h1>Still</h1>
-              </Modal>
-            </Fragment>
-          )}
-        </Toggle>
-      </div>
+      <UserProvider>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <User />
+          <Toggle>
+            {({ on, toggle }) => (
+              <Fragment>
+                <button type="button" onClick={toggle}>
+                  Show/Hide
+                </button>
+                <Modal on={on} toggle={toggle}>
+                  <h1>Still</h1>
+                </Modal>
+              </Fragment>
+            )}
+          </Toggle>
+        </div>
+      </UserProvider>
     );
   }
 }

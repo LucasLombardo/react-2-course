e253485a62b21af4a74a3d56d5e87c4c2b7a062d
Lucas Lombardo <Lucasl@bu.edu>
2018-10-01 08:07:03 -0400

    adds dragging event

--- src/Drag.js
+++ src/Drag.js
@@ -1,12 +1,34 @@
 /* eslint-disable */
 import React, { Component } from "react";
-import { Card } from "Elements";
+import styled from "styled-components";
 import { Gesture } from "react-with-gesture";
 import { Spring, animated, interpolate } from "react-spring";
+import { Card } from "Elements";
 
 const AnimCard = Card.withComponent(animated.div);
 
+const DragCard = AnimCard.extend`
+  height: 300px;
+  /* position: absolute; */
+`;
+
+const CardContainer = styled(animated.div)`
+  position: relative;
+  background: #ccc;
+  max-width: 320px;
+  margin: 0 auto;
+  border-radius: 5px;
+`;
+
 export default class Drag extends Component {
+  onUp = xDelta => () => {
+    if (xDelta < -300) {
+      alert("Remove Card");
+    } else if (xDelta > 300) {
+      alert("Accept Card");
+    }
+  };
+
   render() {
     return (
       <Gesture>
@@ -18,15 +40,41 @@ export default class Drag extends Component {
             }}
             immediate={name => down && name === "x"}
           >
-            {/* ^if the name of the value changing is x and the mouse is down, make response immediate */}
             {({ x }) => (
-              <AnimCard
+              <CardContainer
                 style={{
-                  transform: x.interpolate(x => `translateX(${x}px)`)
+                  background: x.interpolate({
+                    range: [-300, 300],
+                    output: ["#FF1C68", "#14D790"],
+                    extrapolate: "clamp"
+                  })
                 }}
               >
-                <h1>hi</h1>
-              </AnimCard>
+                <DragCard
+                  onMouseUp={this.onUp(xDelta)}
+                  onTouchEnd={this.onUp(xDelta)}
+                  style={{
+                    opacity: x.interpolate({
+                      range: [-300, -100],
+                      output: [0, 1],
+                      extrapolate: "clamp"
+                    }),
+                    transform: interpolate(
+                      [
+                        x,
+                        x.interpolate({
+                          range: [-300, 300],
+                          output: [-45, 45],
+                          extrapolate: "clamp"
+                        })
+                      ],
+                      (x, rotate) => `translateX(${x}px) rotate(${rotate}deg)`
+                    )
+                  }}
+                >
+                  <h1>Drag Me</h1>
+                </DragCard>
+              </CardContainer>
             )}
           </Spring>
         )}
